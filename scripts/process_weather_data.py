34a78227bf6fb6c92d0ea4d56ee750aa31424e36
Markus Ehrlich <markus.ehrlich@cariad.technology>
2025-12-03 17:41:16 +0100

    Added processing for precipitation_sum (standard scaler); added testwise preparation for conversion to jupyter

--- scripts/process_weather_data.py
+++ scripts/process_weather_data.py
@@ -1,20 +1,24 @@
 """Process fetched weather data to make it ready for model training."""
 
-#from datetime import date, timedelta, datetime
 import pathlib
-#import requests
 import json
 import pandas as pd
+#import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
 
 # Paths definition
 DATA_DIR_RAW = pathlib.Path("data/raw")
 DATA_DIR_PROCESSED = pathlib.Path("data/processed")
 DATA_DIR_PROCESSED.mkdir(parents=True, exist_ok=True)
 
-# Daten laden
+
+# Open raw data file
 with open(DATA_DIR_RAW / 'weather_data.json', 'r', encoding='utf-8') as f:
     weatherData = json.load(f)
 
+# %%
 # Transform feedback from API into DataFrame
 df_raw = pd.DataFrame({
     "date": weatherData["daily"]["time"],
@@ -32,34 +36,70 @@ df_raw = pd.DataFrame({
     "snowfall_sum": weatherData["daily"]["snowfall_sum"],
     "precipitation_hours": weatherData["daily"]["precipitation_hours"],
     "weather_code": weatherData["daily"]["weather_code"],
-    "shortwave_radiation_sum": weatherData["daily"]["shortwave_radiation_sum"],
-    "et0_fao_evapotranspiration": weatherData["daily"]["et0_fao_evapotranspiration"]
+    "shortwave_radiation_sum": weatherData["daily"]["shortwave_radiation_sum"], # How much solar radiation reached the ground
+    "et0_fao_evapotranspiration": weatherData["daily"]["et0_fao_evapotranspiration"] # How much water would evaporate, given sufficient water supply
 })
 
+# %%
 # Print head of DataFrame for verification
 print(df_raw.head())
+
+# Collect features
+# Just drop date
+df_features: pd.DataFrame = df_raw.drop(columns=["date"]).copy()
+
+# %%
+# Process features physicallywise
+# Create combined precipitation sum feature and drop individual ones
+df_features["precipitation_sum"] = df_features["rain_sum"] + df_features["snowfall_sum"]
+df_features.drop(columns=["rain_sum", "snowfall_sum"], inplace=True)
+
+# temporary workaround: Numerical values must be dropped until they have been processes properly
+df_features = df_features.drop(columns=["sunset", "sunrise", "weather_code"])
+
+# %%
+# Features for the last available day to predict next day
+# I am not so sure yet, if this will be needed
+df_features_for_tomorrow = df_features.iloc[[-1]] # not used yet
+
+# Delete last row from features dataframe to align with target
+df_features = df_features.drop(df_features.index[-1])
+
+# %%
+# Select target (next day's max temperature)
+df_target = df_raw["temp_max"].shift(-1).to_frame()
+df_target = df_target.dropna()
+
+# Processing for model readability
+# precipitation_sum
+
+#fillna
+#from sklearn.impute import SimpleImputer
+#imputer = SimpleImputer(strategy="median")
+
+# %%
+standard_scaler = StandardScaler()
+df_features["precipitation_sum_scaled"] = standard_scaler.fit_transform(
+    df_features[["precipitation_sum"]])
+df_features = df_features.drop(columns=["precipitation_sum"])
 
 # Placeholder for value conditioning, normalization etc.
 
 # ##################################
 # ##################################
 
-# Split data frame into features and target
-# Drop date and target
-df_features = df_raw.drop(columns=["date", "temp_mean"])
-# temporary workaround: Numerical values must be dropped until they have been processes properly
-df_features = df_features.drop(columns=["sunset", "sunrise", "weather_code"])
+# %%
+# Print overview of processed data
+print("Processed feature data:")
+print(df_features.head())
+print(df_features.info())
+print(df_features_for_tomorrow.head())
+print(df_features_for_tomorrow.info())
+print("Processed target data:")
+print(df_target.head())
+print(df_target.info())
 
-# Features for the last available day to predict next day
-df_features_for_tomorrow = df_features.iloc[[-1]]
-
-# Delete last row from features dataframe to align with target
-df_features = df_features.drop(df_features.index[-1])
-
-# Target is next day's mean temperature
-df_target = df_raw["temp_mean"].shift(-1).to_frame()
-df_target = df_target.dropna()
-
+# %%
 # Save processed data
 processed_data = {
     "features": df_features.to_dict(orient="split"),
